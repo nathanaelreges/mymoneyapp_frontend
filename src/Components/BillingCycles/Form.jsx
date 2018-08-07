bcfc27fe188843956880601ce53f6ecfaa279f94
Nathanael Reges <nathanaelregesdev@gmail.com>
2018-08-07 19:46:44 -0300

    Add tad styled

--- src/Components/BillingCycles/Form.jsx
+++ src/Components/BillingCycles/Form.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { reduxForm, Field, } from 'redux-form'
 
+
+const renderField = ({type, input, label}) => (
+   <div className="form-group">
+      <label htmlFor={input.name}>
+         {label}
+      </label>
+      <input {...input} id={input.name} type={type} className="form-control" placeholder={label}/>
+   </div>
+)
+
 const Form = props => {
    const { handleSubmit, submitting } = props
    return (
-      <form onSubmit={handleSubmit}>
-         <Field name="name" component="input" />
-         <Field name="month" component="input" />
-         <Field name="year" component="input" />
-         <button type="submit" disabled={submitting}>
+      <form className="form" onSubmit={handleSubmit}>
+         <Field name="name" label="Nome" type="text" component={renderField} />
+         <Field name="month" label="Mês" type="number" component={renderField} />
+         <Field name="year" label="Ano" type="number" component={renderField} />
+         <button type="submit" disabled={submitting} className="btn btn-primary">
             Submit
          </button>
       </form>
