8ab54a3e125ea21ae96555551aa9f40103524964
Nathanael Reges <nathanaelregesdev@gmail.com>
2018-08-07 12:56:51 -0300

    Tabs selecting

--- src/common/tab/TabHeader.jsx
+++ src/common/tab/TabHeader.jsx
@@ -6,7 +6,7 @@ import { select } from './actions'
 
 const TabHeader = props => (
    <li className={props.selected == props.target? 'active': ''} >
-      <a href="#" data-toggle={props.target}>
+      <a href="#" data-toggle={props.target} onClick={()=>{props.select(props.target)}}>
          {props.title}
       </a>
    </li>
