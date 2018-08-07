88a09209efefd18f70065a47ceb4643663573049
Nathanael Reges <nathanaelregesdev@gmail.com>
2018-08-07 16:05:52 -0300

    tab imports improved

--- src/Components/BillingCycles/BillingCylcles.jsx
+++ src/Components/BillingCycles/BillingCylcles.jsx
@@ -3,17 +3,12 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
 import Content from '../../common/templates/Content'
-import TabsBox from '../../common/tab/TabsBox'
-import TabsHeader from '../../common/tab/TabsHeader'
-import TabHeader from '../../common/tab/TabHeader'
-import TabsContent from '../../common/tab/TabsContent'
-import TabContent from '../../common/tab/TabContent'
-import { select, show } from '../../common/tab/actions'
+import { TabsBox, TabsHeader, TabHeader, TabsContent, TabContent, TabActions  } from '../../common/tab'
 
 class BillingCycles extends React.Component {
    componentWillMount () {
-      this.props.show('list', 'delete')
-      this.props.select('list')
+      this.props.showTabs('list', 'edit')
+      this.props.selectTab('list')
    }
 
    render() {
@@ -40,7 +35,10 @@ class BillingCycles extends React.Component {
 
 const mapStateToProps = state => ({selected: state.tabs.selected})
 
-const mapDispatchToActions = dispatch => (bindActionCreators({ select, show }, dispatch))
+const mapDispatchToActions = dispatch => (bindActionCreators({ 
+   selectTab: TabActions.select, 
+   showTabs: TabActions.show 
+}, dispatch))
 
 export default connect(mapStateToProps, mapDispatchToActions)(BillingCycles)
 
---
+++ src/common/tab/index.js
@@ -0,0 +1,10 @@
+import TabsBox from './TabsBox'
+import TabsHeader from './TabsHeader'
+import TabHeader from './TabHeader'
+import TabsContent from './TabsContent'
+import TabContent from './TabContent'
+import * as TabActions from './actions'
+
+
+
+export { TabsBox, TabsHeader, TabHeader, TabsContent, TabContent, TabActions  }
